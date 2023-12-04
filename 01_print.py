75fcf8bf30e3f697279174031acc9fab40de4b43
Guillermo Molina <gmolinav2005@gmail.com>
2023-12-03 20:03:45 -0600

    Versión inicial

---
+++ 01_print.py
@@ -0,0 +1,13 @@
+print('Hello World from python')
+
+print('ya vino la ley, para quedarse')
+
+"""
+a continuacion algunas operaciones
+con python
+"""
+
+print (12+5)
+print (12-5)
+print (2 * 3)
+print (8/2)
