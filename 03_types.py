b5e12108057522e7f24a894a5f925829795bf53c
Guillermo Molina <gmolinav2005@gmail.com>
2023-12-03 20:34:43 -0600

    Tipos

---
+++ 03_types.py
@@ -0,0 +1,20 @@
+# Strings
+my_name = 'Memin'
+my_name = "Molina"
+print("my name => ", my_name)
+print(type(my_name))
+
+#Integer
+mi_edad = 56
+print("mi edad => ", mi_edad)
+print(type(mi_edad))
+
+#boolean
+mi_verdad = True
+print("mi verdad => ", mi_verdad)
+print(type(mi_verdad))
+
+#input
+my_agex = input("Cual es su edad? ")
+print("mi edad capturada =>",my_agex)
+print(type(my_agex))
