2fac48f93978ae5c4e866b9f02957e30eaeb920c
Guillermo Molina <gmolinav2005@gmail.com>
2023-12-03 20:25:37 -0600

    Variables

--- 02_vars.py
+++ 02_vars.py
@@ -3,3 +3,19 @@ print("Hola, vars")
 my_name = 'nada'
 
 print (my_name)
+
+my_age = 15
+print(my_age)
+
+my_age = 'quince'
+print(my_age)
+
+print ("ahora tomó este valor en letras " , my_age)
+
+
+"""
+Input
+"""
+mi_dato = input('Cual es su nombre? ')
+
+print("el nombre ingresado es ", mi_dato)
